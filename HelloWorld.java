06edad0337d8c9f0150767df573453b050c0c27a
Shivani <44637756+Shivani1796@users.noreply.github.com>
2020-03-18 21:49:38 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// Assignment-1 Answer
+// Java Example for HEllO-WORLD !!
+
+public class HelloWorld                      // Class name is HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");  // This is print statement
+    }
+
+}
